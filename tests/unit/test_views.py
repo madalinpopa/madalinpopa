742590ca95c3a725fc95901caf98a34a41ad6659
Madalin Popa <github@madalintech.com>
2022-09-09 18:55:05 +0000

    update test for home page view

--- tests/unit/test_views.py
+++ tests/unit/test_views.py
@@ -1,15 +1,15 @@
 """
 Tests for blog views
 """
+from django.test import SimpleTestCase
 from django.urls import reverse
-from pytest_django.asserts import assertTemplateUsed
 
 
-def test_used_template_blog_page(client):
-    """
-    Test the template used.
-    Expected: blog/home.html
-    """
-    expected_template = "blog/home.html"
-    response = client.get(reverse("blog:home"))
-    assertTemplateUsed(response, expected_template)
+class TestBlogViews(SimpleTestCase):
+    """Tests for blog views"""
+
+    def test_used_template_blog_page(self):
+        """Test the template used used by the blog home view"""
+        expected = "blog/home.html"
+        response = self.client.get(reverse("blog:home"))
+        self.assertTemplateUsed(response=response, template_name=expected)
