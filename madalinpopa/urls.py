50bef6a4d36708f62a36a9c594efaa1952bf0a75
Madalin Popa <github@madalintech.com>
2022-09-09 17:02:03 +0000

    update main urls.py, add blog app views

--- madalinpopa/urls.py
+++ madalinpopa/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+import typing
 
-urlpatterns = [
+from django.contrib import admin
+from django.urls import URLPattern, URLResolver, include, path
+
+URL = typing.Union[URLPattern, URLResolver]
+UrlList = typing.List[URL]
+
+# Admin site settings
+admin.site.site_header = "Madalin Popa"
+admin.site.site_title = "Dashboard"
+admin.site.index_title = "Dashboard"
+
+DJANGO_APPS: UrlList = [
     path("admin/", admin.site.urls),
 ]
+
+LOCAL_APPS: UrlList = [
+    path("", include("madalinpopa.apps.blog.urls", namespace="blog")),
+]
+
+urlpatterns: UrlList = DJANGO_APPS + LOCAL_APPS
