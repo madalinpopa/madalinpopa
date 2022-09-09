8d909a34bdd01aaf766e02ff95ef9f192f1c756a
Madalin Popa <github@madalintech.com>
2022-09-09 16:49:01 +0000

    add home view for blog app

--- madalinpopa/apps/blog/views.py
+++ madalinpopa/apps/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""
+Blog App Views
+"""
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.template.response import TemplateResponse
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+
+@require_http_methods(["GET"])
+def home_view(request: HttpRequest) -> HttpRequest:
+    """Blog home page view"""
+    template = "blog/home.html"
+    return TemplateResponse(request=request, template=template)
