65efea08d36dea533041e8a95e2826985c67d2e5
Madalin Popa <contact@madalinpopa.com>
2022-10-12 19:55:25 +0300

    fix: Correct response type

--- madalinpopa/apps/blog/views.py
+++ madalinpopa/apps/blog/views.py
@@ -8,7 +8,7 @@ from django.views.decorators.http import require_http_methods
 
 
 @require_http_methods(["GET"])
-def home_view(request: HttpRequest) -> HttpRequest:
+def home_view(request: HttpRequest) -> HttpResponse:
     """Blog home page view"""
     template = "blog/home.html"
     return TemplateResponse(request=request, template=template)
